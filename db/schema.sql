ab0cab6ccec81f6065c0d92776abf0a5b4c1a290
Iced Coffee <jackbphillips@gmail.com>
2022-10-24 21:01:18 -0400

    updated schema to create the 3 Tables for posts -Jack

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,30 @@
 
 -- CREATE DATABASE
 CREATE DATABASE gamers_db;
+
+USE gamers_db;
+
+CREATE TABLE games (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    gameTitle VARCHAR(64) NOT NULL,
+    Summary VARCHAR(255),
+    onlineStatus BOOLEAN,
+    singlePlayer
+);
+
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50),
+    userName VARCHAR(50) NOT NULL,
+    passwordHash VARCHAR(128) NOT NULL,
+    email VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE game_posts (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (gameID) REFERENCES games(id),
+    FOREIGN KEY (userID) REFERENCES users(id),
+    messageContent VARCHAR(255) NOT NULL,
+    createdAT timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
