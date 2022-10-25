2db5654134b9d0d097a3dd51cc54179ce8c5a60d
Ayoub <biliad8786@gmail.com>
2022-10-25 18:47:03 -0400

    Minor changes to controllers - Ayoub

---
+++ controllers/api/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+const postRoutes = require('./post');
+
+module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+//router.use('/', );
+router.use('/api', apiRoutes);
+
+module.exports = router;
