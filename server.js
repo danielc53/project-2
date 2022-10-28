e90b4e50002362ffd7554bf59b4827b1aeaea082
reyno405 <reyno405@gmail.com>
2022-10-27 20:40:57 -0400

    has middle ware for session and cookies

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+// Import express-session
+const session = require('express-session');
+
 const dotenv = require('dotenv');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -14,6 +17,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+// Set up sessions
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 3600000,
+    httpOnly: true,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  // Sets up session store
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
