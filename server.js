531cf20e030dbd9877b311928210b9d96bc2ec76
isain <ibarrais@msu.edu>
2022-10-27 21:24:10 -0400

    Added handlebars to server.js and created layouts folder with main.handlebars and started a rough draft for user-profile.handlebars -Isain

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
