25f3a4c66e9f20511fd225a6c2da083fe2d98fb8
danielc53 <dannyc53@hotmail.com>
2022-12-15 17:50:37 -0500

    edit env and conncetion

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: 'mysql',
+//     port: 3306,
+//   }
+// );
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+  process.env.CLEARDB_DATABASE_URL
+)
 
 module.exports = sequelize;
